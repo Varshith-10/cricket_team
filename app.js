a54e7fadb5623952201fbeed9a426e58a3603c3e
Varshith-10 <156418378+Varshith-10@users.noreply.github.com>
2024-01-20 20:30:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/players/', async (request, response) => {
   FROM 
   cricket_team 
   ORDER BY 
-  player_Id; `
+  player_id;`
   const playersArray = await db.all(playersAll)
   response.send(playersArray)
 })
@@ -40,19 +40,18 @@ app.post('/players/', async (request, response) => {
   const playersDetails = request.body
   const {playerName, jerseyNumber, role} = playersDetails
   const playerAdd = `INSERT INTO cricket_team (player_name, jersey_number, role) VALUES(
-    `${Vishal}`,
-    ${17},
-    `${Bowler}`
+    ${playerName}, 
+    ${jerseyNumber},
+    ${role},
   );`
   const res = await db.run(playerAdd)
   const playerId = res.lastID
-  response.send({playerId: playerId})
   response.send('Player Added to Team')
 })
 
 app.get('/players/:playerId/', async (request, response) => {
   const {playerId} = request.params
-  const playerGet = `SELECT * FROM players WHERE playerId = ${playerId};`
+  const playerGet = `SELECT * FROM cricket_team WHERE player_id = ${playerId};`
   const playerone = await db.get(playerGet)
   response.send(playerone)
 })
@@ -61,9 +60,9 @@ app.put('/players/:playerId/', async (request, response) => {
   const {playerId} = request.params
   const playersDetails = request.body
   const {playerName, jerseyNumber, role} = playersDetails
-  const playerUpdt = `UPDATE players SET
-   Maneesh =${playerName},
-   54 =${jerseyNumber},
+  const playerUpdt = `UPDATE cricket_team SET
+   player_name = ${playerName},
+   jersey_number = ${jerseyNumber},
    All-Rounder = ${role}
    WHERE playerId = ${playerId};`
   await db.run(playerUpdt)
@@ -72,7 +71,7 @@ app.put('/players/:playerId/', async (request, response) => {
 
 app.delete('/players/:playerId/', async (request, response) => {
   const {playerId} = request.params
-  const playerDel = `DELETE FROM players WHERE playerId = ${playerId};`
+  const playerDel = `DELETE FROM cricket_team WHERE playerId = ${playerId};`
   await db.run(playerDel)
   response.send('Player Removed')
 })
